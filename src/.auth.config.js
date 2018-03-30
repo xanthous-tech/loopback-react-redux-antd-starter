169890f41e9e21dd18b6ee91e85cbcf8d3b80846
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2018-03-30 23:01:40 +0800

    fix hardcoded

--- src/.auth.config.js
+++ src/.auth.config.js
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oaei5a0i314xQSJw0h7',
     issuer: 'https://dev-859469.oktapreview.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/implicit/callback',
+    redirectUri: window.location.origin + '/implicit/callback',
     scope: 'openid profile email',
   },
   resourceServer: {
